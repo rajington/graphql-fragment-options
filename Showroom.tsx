fba7bf3725a816c0ce65a8f8d40145774515d8f8
Rajarshi Nigam <raj@onepeloton.com>
2020-04-01 12:56:51 -0400

    use graphql-anywhere

--- Showroom.tsx
+++ Showroom.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { filter } from "graphql-anywhere";
 import Hero from "./Hero";
+import HeroFragment from "./Hero.graphql";
 import Gallery from "./Gallery";
+import GalleryFragment from "./Gallery.graphql";
 import GoogleMaps from "./GoogleMaps";
+import GoogleMapsFragment from "./GoogleMaps.graphql";
 import { ShowroomQueryVariables, useShowroomQuery } from "./Showroom.graphql";
 
 const Showroom: React.FC<ShowroomQueryVariables> = variables => {
@@ -9,9 +13,9 @@ const Showroom: React.FC<ShowroomQueryVariables> = variables => {
 
   return (
     <>
-      <Hero {...data} />
-      <Gallery {...data} />
-      <GoogleMaps {...data.location} />
+      <Hero {...filter(data, HeroFragment)} />
+      <Gallery {...filter(data, GalleryFragment)} />
+      <GoogleMaps {...filter(data.location, GoogleMapsFragment)} />
     </>
   );
 };
